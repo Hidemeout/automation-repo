8de1fdad68a8b8fa4b3f6fe1d264dd21567b5bd1
Hidemeout <ghostmaker29539@gmail.com>
2025-11-27 08:43:54 +0200

    Create check_load.sh

---
+++ scripts/check_load.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+LOG=/opt/automation/logs/load.log
+LOAD=$(awk '{print $1}' /proc/loadavg)
+THRESH=4.0
+if (( $(echo "$LOAD > $THRESH" | bc -l) )); then
+echo "$(date): High load ($LOAD) - restarting heavy services" >> "$LOG"
+systemctl restart pritunl
+systemctl restart mongod
+fi

14ad2e40bbbc9774b16717c2d689ef862a33356f
Hidemeout <ghostmaker29539@gmail.com>
2025-12-02 14:08:43 +0200

    Update backup-mongo.sh

--- scripts/backup-mongo.sh
+++ scripts/backup-mongo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
+
 OUT_DIR=/opt/backups/mongo
 mkdir -p "$OUT_DIR"
 OUT="$OUT_DIR/mongo-$(date +%F-%H%M).gz"
+
 /usr/bin/mongodump --archive="$OUT" --gzip
-# keep 7 days
+
 find "$OUT_DIR" -type f -mtime +7 -delete

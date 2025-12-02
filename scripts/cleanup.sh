05adfc912bb158655e4d9adc07a1108eac1e66d2
Hidemeout <ghostmaker29539@gmail.com>
2025-12-02 14:10:32 +0200

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-# Очистка журналов
+
 journalctl --vacuum-size=200M
-# Удаление неиспользуемых пакетов
+
 apt autoremove -y
 apt clean -y
-# Docker cleanup (если есть)
+
 docker system prune -af || true

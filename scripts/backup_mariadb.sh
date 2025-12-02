a2cc89e7a67a9f8f0ea1dcfafd0ac03b7446a8cd
Hidemeout <ghostmaker29539@gmail.com>
2025-12-02 14:08:58 +0200

    Update backup_mariadb.sh

--- scripts/backup_mariadb.sh
+++ scripts/backup_mariadb.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
+
 OUT_DIR=/opt/backups/mariadb
 mkdir -p "$OUT_DIR"
+
 OUT="$OUT_DIR/mariadb-$(date +%F-%H%M).sql.gz"
 mysqldump --all-databases | gzip > "$OUT"
+
 find "$OUT_DIR" -type f -mtime +7 -delete

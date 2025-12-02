55c3a0a83533e57634332c85c0b08fb526e2eddd
Hidemeout <ghostmaker29539@gmail.com>
2025-12-02 14:09:37 +0200

    Update check_pritunl.sh

--- scripts/check_pritunl.sh
+++ scripts/check_pritunl.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Проверяет статус pritunl и рестартует если нужно
+
 LOG=/opt/automation/logs/pritunl.log
+
 if ! systemctl is-active --quiet pritunl; then
-echo "$(date): Pritunl is down, restarting..." >> "$LOG"
-systemctl restart pritunl
+  echo "$(date): Pritunl is down, restarting..." >> "$LOG"
+  systemctl restart pritunl
 fi

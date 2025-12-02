4655cb80ac39f32bc8a9a5495d8b346445b18ea4
Hidemeout <ghostmaker29539@gmail.com>
2025-12-02 14:09:17 +0200

    Update backup_pritunl.sh

--- scripts/backup_pritunl.sh
+++ scripts/backup_pritunl.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
+
 OUT_DIR=/opt/backups/pritunl
 mkdir -p "$OUT_DIR"
+
 OUT="$OUT_DIR/pritunl-$(date +%F-%H%M).tar"
 pritunl export-config "$OUT"
+
 find "$OUT_DIR" -type f -mtime +7 -delete
